477d7f071773ce5488390c04f22d76ef8978e51f
griogrii_riabov <garyabov@mts.ru>
2026-08-03 10:26:06 +0400

    refactor(adapter): preserve hotspot ratchet

--- packages/adapter-sdk/src/index.mjs
+++ packages/adapter-sdk/src/index.mjs
@@ -145,8 +145,7 @@ let stderr = "";
 let timedOut = false;
 let interrupted = false;
 let emitted = false;
-let heartbeatTimer = null;
-let processTreeSnapshotTimer = null;
+let heartbeatTimer = null, processTreeSnapshotTimer = null;
 let interruptKillTimer = null;
 let timeoutTimer = null;
 let stdoutLineBuffer = "";
@@ -236,10 +235,7 @@ function finish(result) {
     clearInterval(heartbeatTimer);
     heartbeatTimer = null;
   }
-  if (processTreeSnapshotTimer) {
-    clearInterval(processTreeSnapshotTimer);
-    processTreeSnapshotTimer = null;
-  }
+  if (processTreeSnapshotTimer) clearInterval(processTreeSnapshotTimer);
   if (interruptKillTimer) {
     clearTimeout(interruptKillTimer);
     interruptKillTimer = null;
@@ -290,9 +286,7 @@ try {
   });
   process.exit(0);
 }
-
 processTreeSnapshotTimer = trackProcessTree(child, timeoutMs);
-
 heartbeatTimer = setInterval(() => {
   if (providerCancellationRequested()) {
     if (!interrupted) {
